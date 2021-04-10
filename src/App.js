f2ea5403286f4d291062663be60cb85739e14ca7
bawumbila <74751215+bawumbila@users.noreply.github.com>
2021-04-10 13:42:32 -0400

    WIP updates

--- src/App.js
+++ src/App.js
@@ -5,20 +5,15 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>Waves</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>WAVe Music App</h1>
       </header>
+      <body>
+        
+      <button>Search</button>
+      </body>
     </div>
+    
+    
   );
 }
 
